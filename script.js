14f7d126d2df0a0415ac339f80da93f6c4a098fb
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-22 23:17:57 -0800

    for-loop-js

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("connected")
+
+// for loops
+
+//  for(let i = 0; i < 5; i++ ){
+     
+//  }
+const names = [ 'Myco', 'Paul' ,'Fred'];
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+ 
